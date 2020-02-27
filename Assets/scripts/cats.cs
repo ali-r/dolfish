805785b75de9fab5ea99eefed1b4e2be2504f6da
unknown <ali.r.hariri@gmail.com>
2020-02-27 13:02:16 +0330

    adding categorys

--- Assets/scripts/cats.cs
+++ Assets/scripts/cats.cs
@@ -2,14 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using System;
+
+
 public class cats : MonoBehaviour
 {
     // Start is called before the first frame update
+
     void Start()
     {
+        float r = 2f;
+        for (int i = 0; i <= 5; i++) 
+        {
+            GameObject cat = this.transform.GetChild(i).gameObject; 
+            Vector3 pos = cat.transform.position;
+            pos.x = r * (float)Math.Sin(i* 2*Math.PI / 6);
+            pos.y = r * (float)Math.Cos(i* 2*Math.PI / 6);
+            cat.transform.position = pos;
+            Debug.Log(pos);
+        }
         Debug.Log("hello");
-        Debug.Log((this.cat1).ToString());
-        
 
     }
 

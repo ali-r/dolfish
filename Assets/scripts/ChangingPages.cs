22d6f907ba799d463548de78bb45eccfcb5e0a95
Sadegh <s.m.torabi92@gmail.com>
2020-05-12 01:00:15 +0430

    changing pages added

---
+++ Assets/scripts/ChangingPages.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangingPages : MonoBehaviour
+{
+
+    [SerializeField] float SceneDelay = 5f;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Play");
+        //FindObjectOfType<GameSession>().Reset();
+    }
+    public void MainMeneu()
+    {
+        SceneManager.LoadScene(0);
+        //FindObjectOfType<GameSession>().Reset();
+    }
+    public void Panel()
+    {
+        SceneManager.LoadScene("Panel");
+        //FindObjectOfType<GameSession>().Reset();
+    }
+    public void Store()
+    {
+        SceneManager.LoadScene("Store");
+        //FindObjectOfType<GameSession>().Reset();
+    }
+    public void GameOver()
+    {
+        StartCoroutine(Delay());
+
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(SceneDelay);
+        SceneManager.LoadScene("GameOver");
+
+    }
+}

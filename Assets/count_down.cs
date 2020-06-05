b5b821928550f1a7acf915a8f323bf6f7ab79ceb
alireza <ali.r.hariri@gmail.com>
2020-06-05 08:34:08 +0430

    countdown

---
+++ Assets/count_down.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TMPro;
+
+public class count_down : MonoBehaviour
+{
+    // Start is called before the first frame update
+	
+	persist_state state;
+	
+	private TextMeshProUGUI TMP;
+	
+    void Start()
+    {
+        state = GameObject.Find("persist_State").GetComponent<persist_state>();
+		TMP = GetComponent<TextMeshProUGUI>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		var tmp = state.remainedSortTime.ToString("00.0").Split('.');
+		TMP.text = tmp[0]+":"+tmp[1];
+    }
+}
